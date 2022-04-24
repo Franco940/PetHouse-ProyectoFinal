55d3b0161766da31af723026a16c88e7f1a2e4be
Franco940 <franco.3271940@gmail.com>
2022-04-23 23:01:54 -0300

    First upload

---
+++ src/test/java/com/proyectoFinal/PetHouse/PetHouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectoFinal.PetHouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetHouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
