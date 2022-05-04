9247178498aa0a69439af1f11da57525e78ff629
victoriaserrano698 <victoriaserrano698@gmail.com>
2022-05-04 18:44:37 -0300

    ya se puede ir de home a registro y de registro a home

--- src/main/java/com/proyectoFinal/PetHouse/controladores/UsuarioControlador.java
+++ src/main/java/com/proyectoFinal/PetHouse/controladores/UsuarioControlador.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -24,16 +25,20 @@ public class UsuarioControlador {
     @Autowired
     private UsuarioServicio us;
 
-    @GetMapping("/registrar")
+    @PostMapping("/registrar")
     public String guardar(ModelMap modelo, @RequestParam String nombre, @RequestParam String apellido, @RequestParam String email, @RequestParam String contrasenia, @RequestParam Integer telefonoDeContacto, @RequestParam String ubicacion) {
         try {
             us.registrarUsuario(nombre, apellido, email, contrasenia, telefonoDeContacto, ubicacion);
             modelo.put("exito", "Registro exitoso");
-            return "registrar-usuario";
+            return "form-registro";
         } catch (Exception e) {
             modelo.put("error", "Falta algún dato");
-            return "registrar-usuario";
+            return "form-registro";
         }
+    }
+    @GetMapping("/registrar")
+    public String mostrarFormulario(){
+        return "form-registro";
     }
 
 }
