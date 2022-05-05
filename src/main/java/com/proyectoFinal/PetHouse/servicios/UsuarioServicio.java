b4e68ef75b912ade522181fa8fcaf12106ff75c4
victoriaserrano698 <victoriaserrano698@gmail.com>
2022-05-05 17:26:12 -0300

    hice los metodos de modificar y eliminar en usuario servicio

--- src/main/java/com/proyectoFinal/PetHouse/servicios/UsuarioServicio.java
+++ src/main/java/com/proyectoFinal/PetHouse/servicios/UsuarioServicio.java
@@ -14,7 +14,8 @@ public class UsuarioServicio {
     private UsuarioRepositorio ur;
 
     @Transactional
-    public void registrarUsuario(String nombre, String apellido, String email, String contrasenia, Integer telefonoDeContacto, String localidad, String calleNumero) throws Exception {
+    public void registrarUsuario(String nombre, String apellido, String email, String contrasenia,
+            Integer telefonoDeContacto, String localidad, String calleNumero) throws Exception {
         Usuario usuario = new Usuario();
         validaciones(nombre, apellido, email, contrasenia, telefonoDeContacto, calleNumero);
         usuario.setNombre(nombre);
@@ -26,25 +27,47 @@ public class UsuarioServicio {
         usuario.setRol(Rol.USER);
         ur.save(usuario);
     }
-    
-    private void validaciones(String nombre, String apellido, String email, String contrasenia, Integer telefonoDeContacto, String calleNumero)throws Exception{
-        if(nombre == null || nombre.trim().isEmpty()){
+
+    @Transactional
+    public void modificarUsuario(String id, String nombre, String apellido, String email, String contrasenia,
+            Integer telefonoDeContacto, String localidad, String calleNumero) throws Exception {
+        validaciones(nombre, apellido, email, contrasenia, telefonoDeContacto, calleNumero);
+        Usuario usuario = ur.buscarPorId(id);
+        if (usuario != null) {
+            usuario.setNombre(nombre);
+            usuario.setApellido(apellido);
+            usuario.setEmail(email);
+            usuario.setContrasenia(contrasenia);
+            usuario.setTelefonoDeContacto(telefonoDeContacto);
+            usuario.setUbicacion(calleNumero + ", " + localidad + ", Buenos Aires, Argentina");
+            ur.save(usuario);
+        }
+    }
+
+    @Transactional
+    public void eliminarUsuario(String id) throws Exception {
+        ur.buscarPorId(id);
+        ur.deleteById(id);
+    }
+
+    private void validaciones(String nombre, String apellido, String email, String contrasenia, Integer telefonoDeContacto, String calleNumero) throws Exception {
+        if (nombre == null || nombre.trim().isEmpty()) {
             throw new Exception("El nombre no puede estar vacío");
         }
-        if(apellido == null || apellido.trim().isEmpty()){
+        if (apellido == null || apellido.trim().isEmpty()) {
             throw new Exception("El apellido no puede estar vacío");
         }
-        if(email == null || email.trim().isEmpty()){
+        if (email == null || email.trim().isEmpty()) {
             throw new Exception("El email no puede estar vacío");
         }
-         if(contrasenia == null || contrasenia.trim().isEmpty()){
+        if (contrasenia == null || contrasenia.trim().isEmpty()) {
             throw new Exception("La contraseña no puede estar vacía");
         }
-        if (telefonoDeContacto == null){
+        if (telefonoDeContacto == null) {
             throw new Exception("El teléfono no puede ser nulo");
         }
-        if(calleNumero == null || calleNumero.trim().isEmpty()){
+        if (calleNumero == null || calleNumero.trim().isEmpty()) {
             throw new Exception("La calle no puede estar vacía");
         }
-     }
+    }
 }
