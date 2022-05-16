5674ef7e4f9c72db0351adb022d5c41bfaaa65e1
Franco940 <franco.3271940@gmail.com>
2022-05-16 05:11:27 -0300

    Se creo el método para actualizar los datos de un registro.

--- src/main/java/com/proyectoFinal/PetHouse/repositorios/UsuarioRepositorio.java
+++ src/main/java/com/proyectoFinal/PetHouse/repositorios/UsuarioRepositorio.java
@@ -1,22 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.proyectoFinal.PetHouse.repositorios;
 
 import com.proyectoFinal.PetHouse.entidades.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-/**
- *
- * @author VIC
- */
+
 @Repository
 public interface UsuarioRepositorio extends JpaRepository<Usuario, String>{
+    
+    @Modifying
+    @Query("UPDATE Usuario AS u SET u.nombre = :nombre, u.apellido = :apellido, u.email = :email, "
+            + "u.contrasenia = :contrasenia, u.telefonoDeContacto = :telefono, u.ubicacion = :ubicacion, "
+            + "u.fotoDePerfil = :foto WHERE u.idUsuario = :id")
+    public void actualizar(@Param("id") String id, @Param("nombre") String nombre, @Param("apellido") String apellido, 
+            @Param("email") String email, @Param("contrasenia") String contrasenia, @Param("telefono") Integer telefono, 
+            @Param("ubicacion") String ubicacion, @Param("foto") String foto);
+    
     @Query("SELECT u FROM Usuario u WHERE u.idUsuario = :idUsuario")
     public Usuario buscarPorId(@Param("idUsuario") String idUsuario);
     
